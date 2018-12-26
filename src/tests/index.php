064938c5b7d1520a259ef66df4bdcb54566529b0
Lucas <lucas.pereira_oli@hotmail.com>
2018-12-26 21:35:35 -0200

    Meu primeiro commit e nova Branch

--- src/tests/index.php
+++ src/tests/index.php
@@ -1,9 +1,9 @@
 <?php
 
-use PhpIbgeKit\Src\Kits\Places\UfSearch;
+// use PhpIbgeKit\Src\Kits\Places\UfSearch;
 
-$ufSearch = new UfSearch();
-//echo var_dump($ufSearch->getAll());
-echo $ufSearch->getOne(12);
-
+// $ufSearch = new UfSearch();
+// //echo var_dump($ufSearch->getAll());
+// echo $ufSearch->getOne(12);
+echo "Olá";
 
